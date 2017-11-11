a33dec7157d54f019370b384e23a640c8dac0213
salahuddin ansari <sansari150894@gmail.com>
2017-11-12 03:25:27 +0530

    'UI'

--- server/server.js
+++ server/server.js
@@ -22,11 +22,7 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', generateMessage(message.from, message.text));
-    // socket.broadcast.emit('newMessage', {
-    //   from: message.from,
-    //   text: message.text,
-    //   createdAt: new Date().getTime()
-    // });
+    // socket.broadcast.emit('newMessage',generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', () => {
