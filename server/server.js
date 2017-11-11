baf2de9007b5383f8905686adf4493d38df16adf
salahuddin ansari <sansari150894@gmail.com>
2017-11-11 23:28:37 +0530

    newMessage CreateMessage

--- server/server.js
+++ server/server.js
@@ -1,11 +1,33 @@
 const path=require('path');
 const express=require('express');
+const socketIO=require('socket.io');
+const http=require('http');
 const publicPath= path.join(__dirname,'/../public');
 const port=process.env.PORT||3000;
 
 
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
 app.use(express.static(publicPath));
-app.listen(port,()=>{
+
+
+io.on('connection',(socket)=>{
+    console.log("new user is connected");
+    socket.on('disconnect',()=>{
+        console.log("user is disconneted");
+    })
+
+    socket.emit('newMessage',{
+        to:"client",
+        text:"client lo bhai"
+    })
+
+    socket.on('createMessage',(message)=>{
+        console.log("Message:"+message.toString);
+    })
+})
+
+server.listen(port,()=>{
     console.log(`server is running on ${port}`);
 })
