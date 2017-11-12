510685e7ea23b3a6e84bcf6f077365bac7d0cbd5
salahuddin ansari <sansari150894@gmail.com>
2017-11-12 20:49:00 +0530

    'addingJoinPage4'

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,11 @@
 var socket = io();
 
+var param;
+
 socket.on('connect', function () {
   console.log('Connected to server');
-  socket.emit('newMessage',{
-      from : 'salahuddin',
-      text:'this is from salahuddin'
-  });
+  param=jQuery.deparam(window.location.search);
+  console.log(param);
 });
 
 socket.on('disconnect', function () {
@@ -23,9 +23,19 @@ jQuery('#message-form').on('submit',function(e){
     console.log("this is called when submit");
     e.preventDefault();
     socket.emit('createMessage',{
-        from:"user",
+        from:param.name,
         text:jQuery('[name=message]').val(),
         
     });
     jQuery('[name=message]').val('');
+});
+
+
+////////////////////////////////////////////////////////////
+
+jQuery('#message-form-welcome').on('submit',function(e){
+  e.preventDefault();
+  var name =jQuery('[name=message-welcome]').val();
+  console.log(name);
+  
 });
